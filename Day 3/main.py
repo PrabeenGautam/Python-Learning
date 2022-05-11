bc7230149e8da416491bc083a2803d7ea13b8cef
PrabinGautam <prabeen122@gmail.com>
2022-05-11 13:04:47 +0545

    Day 3

---
+++ Day 3/main.py
@@ -0,0 +1,45 @@
+# 🚨 Don't change the code below 👇
+# number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+# number = 9
+# if(number % 2 == 0):
+#     print("Given Number is even")
+# else:
+#     print("Given Number is odd")
+
+# height = float(input("What is your height?\n"))
+# weight = float(input("What is your weight?\n"))
+
+# bmi = round(weight / (height ** 2), 2)
+# if(bmi < 18.5):
+#   print("You are underweight")
+# elif bmi >= 18.5 and bmi < 25:
+#   print("You have normal weight")
+# elif bmi >= 25 and bmi < 30:
+#   print("You are overweight")
+# elif bmi >= 30 and bmi < 35:
+#   print("You are obese")
+# else:
+#   print("You are clinically obese")
+
+# 🚨 Don't change the code below 👇
+# print("Welcome to love calculator")
+
+# name1 = input("What is your name? \n")
+# name2 = input("What is her/his name? \n")
+# combined_name = name1 + name2
+
+# t_count = 0
+# r_count = 0
+# u_count = 0
+# e_count = 0
+# total_count = 0;
+
+# l_count = 0
+# o_count = 0
+# v_count = 0
+# e_count = 0
+
+
